944aaccf374d689c804e5e7d69baa00a7c58166e
YoussefDaoud <youssefdaoud0122@gmail.com>
2024-02-02 23:48:55 +0200

    make encoding done by static key temporary

--- src/security/security.service.ts
+++ src/security/security.service.ts
@@ -4,12 +4,19 @@ import { createCipheriv, randomBytes, scrypt } from 'crypto';
 import { promisify } from 'util';
 import { createDecipheriv } from 'crypto';
 import * as crypto from 'crypto';
+import { env } from 'process';
 
 @Injectable()
 export class SecurityService {
   private readonly algorithm: string = 'aes-256-cbc';
-  private readonly key: Buffer = crypto.randomBytes(32);
-  private readonly iv: Buffer = crypto.randomBytes(16);
+  private readonly key: Buffer = Buffer.from(
+    '60d5aa20b88ea062d25445b49c86d6f49b791cc9f8c435ef86d3c067c93af60f',
+    'hex',
+  );
+  private readonly iv: Buffer = Buffer.from(
+    '6ac1bd1143259fc9084d8a8a4cd63be2',
+    'hex',
+  );
   async HashPassword(password: string) {
     const saltOrRounds = 10;
     const hash = await bcrypt.hash(password, saltOrRounds);
@@ -20,10 +27,14 @@ export class SecurityService {
     return isMatch;
   }
   encrypt(text: string): string {
-    const cipher = crypto.createCipheriv(this.algorithm, this.key, this.iv);
-    let encrypted = cipher.update(text, 'utf-8', 'hex');
-    encrypted += cipher.final('hex');
-    return encrypted;
+    try {
+      const cipher = crypto.createCipheriv(this.algorithm, this.key, this.iv);
+      let encrypted = cipher.update(text, 'utf-8', 'hex');
+      encrypted += cipher.final('hex');
+      return encrypted;
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   decrypt(encryptedText: string): string {
